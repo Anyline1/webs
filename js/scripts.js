453a1b619deeee1407e892fa04902ee439aba024
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-02 17:40:57 +0300

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,32 @@ function fetchTopHeadlinesRU() {
         });
 }
 
+async function fetchWeather() {
+    const weatherElement = document.getElementById("weather");
+    const apiKey = "25e38454a0f2af6bc314bc8b76dc55b1";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Saint Petersburg&appid=${apiKey}&units=metric&lang=ru`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("Ошибка при получении данных о погоде");
+        const data = await response.json();
+
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+
+        weatherElement.innerHTML = `
+            <img src="http://openweathermap.org/img/wn/${icon}.png" alt="${description}">
+            ${temperature}°C, ${description}
+        `;
+    } catch (error) {
+        console.error("Ошибка при получении погоды:", error);
+        weatherElement.textContent = "Не удалось загрузить погоду";
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    fetchWeather();
     fetchTopHeadlinesUS();
     fetchTopHeadlinesRU();
 });
