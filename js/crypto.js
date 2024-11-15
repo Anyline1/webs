70121d8d4fdda9d15ed0375fe977001081b6bfa5
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-15 11:18:45 +0300

    //

--- js/crypto.js
+++ js/crypto.js
@@ -2,7 +2,7 @@ async function fetchCryptoPrices() {
     try {
         const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1';
         const response = await fetch(url, { method: 'GET' });
-        
+
         if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
         const data = await response.json();
