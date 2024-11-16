b86ae54e23984612f2619524d020c7ac4ddc4a0a
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-16 21:08:35 +0300

    theme

--- js/button.js
+++ js/button.js
@@ -1,17 +1,10 @@
-const themeToggle = document.getElementById("theme-toggle");
-
-themeToggle.addEventListener("click", function() {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
-    }
-});
-
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-} else {
-    document.body.classList.remove("dark");
-}
+// const toggleButton = document.querySelector('.theme-toggle');
+// const themeLink = document.getElementById('theme-style');
+//
+// toggleButton.addEventListener('click', () => {
+//     if (themeLink.getAttribute('href') === 'css/style.css') {
+//         themeLink.setAttribute('href', 'css/night.css');
+//     } else {
+//         themeLink.setAttribute('href', 'css/style.css');
+//     }
+// });
