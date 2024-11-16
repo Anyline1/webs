8f1106bfd4f9000b13bac2cbf3a7a6d652267fcc
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-16 20:50:18 +0300

    currency

--- js/currency.js
+++ js/currency.js
@@ -4,12 +4,12 @@ async function fetchCurrencyRates() {
     const cnyElement = document.getElementById("cny-rate");
 
     try {
-        const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+        const response = await fetch('https://api.exchangerate-api.com/v4/latest/RUB');
         if (!response.ok) throw new Error("Ошибка при получении курсов валют");
         const data = await response.json();
 
         const rates = data.rates;
-        usdElement.textContent = `USD: 1.00`;
+        usdElement.textContent = `USD: ${(1 / rates.USD).toFixed(2)}`;
         eurElement.textContent = `EUR: ${(1 / rates.EUR).toFixed(2)}`;
         cnyElement.textContent = `CNY: ${(1 / rates.CNY).toFixed(2)}`;
     } catch (error) {
