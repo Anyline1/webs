6bee08b2764280770fe33ef4b18619288e470f62
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-19 10:21:24 +0300

    fix

--- js/button.js
+++ js/button.js
@@ -2,12 +2,9 @@ const toggleButton = document.querySelector('.theme-toggle');
 const themeLink = document.getElementById('theme-style');
 
 toggleButton.addEventListener('click', () => {
-    console.log('Button clicked!');
     if (themeLink.getAttribute('href') === 'css/style.css') {
         themeLink.setAttribute('href', 'css/night.css');
-        console.log('Switched to night.css');
     } else {
         themeLink.setAttribute('href', 'css/style.css');
-        console.log('Switched to style.css');
     }
 });
