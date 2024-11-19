0e1ccb7a990fb44006d490daeae5f25287f4386f
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-11-19 10:21:17 +0300

    fix

--- js/button.js
+++ js/button.js
@@ -1,10 +1,13 @@
-// const toggleButton = document.querySelector('.theme-toggle');
-// const themeLink = document.getElementById('theme-style');
-//
-// toggleButton.addEventListener('click', () => {
-//     if (themeLink.getAttribute('href') === 'css/style.css') {
-//         themeLink.setAttribute('href', 'css/night.css');
-//     } else {
-//         themeLink.setAttribute('href', 'css/style.css');
-//     }
-// });
+const toggleButton = document.querySelector('.theme-toggle');
+const themeLink = document.getElementById('theme-style');
+
+toggleButton.addEventListener('click', () => {
+    console.log('Button clicked!');
+    if (themeLink.getAttribute('href') === 'css/style.css') {
+        themeLink.setAttribute('href', 'css/night.css');
+        console.log('Switched to night.css');
+    } else {
+        themeLink.setAttribute('href', 'css/style.css');
+        console.log('Switched to style.css');
+    }
+});
