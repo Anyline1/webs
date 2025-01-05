45870ecf17ba60ff1dfbb954f12b67d03e08f73e
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2025-01-05 21:44:45 +0300

    //

--- js/slottest.js
+++ js/slottest.js
@@ -1,22 +1,3 @@
-const mockElements = {
-    balance: { textContent: '1000' },
-    betAmount: { value: '500' },
-    message: { textContent: '' },
-};
-
-const mockFunctions = {
-    updateBalance: jest.fn(),
-    checkWinWithBet: jest.fn(),
-};
-
-jest.mock('./slot', () => ({
-    ...jest.requireActual('./slot'),
-    document: {
-        getElementById: id => mockElements[id],
-    },
-    updateBalance: mockFunctions.updateBalance,
-    checkWinWithBet: mockFunctions.checkWinWithBet,
-}));
 
 
 
